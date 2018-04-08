d054fa7cda18f45affc70445fac5d68c7f6af5d8
Todor Angelov <tondy@tondy.com>
2018-04-08 15:26:06 +0200

    Книгите

---
+++ bg/Книгите/meta.js
@@ -0,0 +1,9 @@
+const meta = `
+{
+	"sort"  : 2,
+	"name": "Тондуций",
+	"desc": "Аз съм Тондуций. Шегувам се",
+	"logo": "logo.png",
+	"tags": ["bg","2018","AbvOS","featured"]
+}
+`;module.exports = meta;
